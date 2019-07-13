3c818bb14be5ef67718079e7664fc3f83752afa1
Joi Liu <547169735@qq.com>
2019-07-13 22:54:42 +0800

    finish Flaten.java

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -2,20 +2,24 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Flaten {
 
-    Integer[][] array;
-    public Flaten(Integer[][] array) {
-        this.array = array;
-    }
+  Integer[][] array;
 
-    public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
-    }
+  public Flaten(Integer[][] array) {
+    this.array = array;
+  }
 
-    public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
-    }
+  public List<Integer> transformToOneDimesional() {
+    return Arrays.stream(array).flatMap(x -> Stream.of(x)).collect(Collectors.toList());
+  }
+
+  public List<Integer> transformToUnrepeatedOneDimesional() {
+    return Arrays.stream(array).flatMap(x -> Stream.of(x)).distinct().collect(Collectors.toList());
+  }
 }
